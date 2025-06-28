1444412db6150a73ebf208d75e8a6906a4f9dfd4
Fei <xu_neu@163.com>
2025-06-29 02:29:38 +0800

    <feat>[write]:卷章管理

---
+++ src/main/java/cn/timflux/storyseek/core/write/dto/VolumeChapterCreateDTO.java
@@ -0,0 +1,19 @@
+package cn.timflux.storyseek.core.write.dto;
+
+import lombok.Data;
+
+/**
+ * ClassName: VolumeChapterCreateDTO
+ * Package: cn.timflux.storyseek.core.write.dto
+ * Description:
+ *
+ * @Author 一剑霜寒十四州
+ * @Create 2025/6/25 下午4:10
+ * @Version 1.0
+ */
+@Data
+public class VolumeChapterCreateDTO {
+    private Long bookId;
+    private Long parentId;  // 卷的parentId可不传或者传0，章必须传卷的id
+    private String name;
+}
---
+++ src/main/java/cn/timflux/storyseek/core/write/dto/VolumeChapterRenameDTO.java
@@ -0,0 +1,18 @@
+package cn.timflux.storyseek.core.write.dto;
+
+import lombok.Data;
+
+/**
+ * ClassName: VolumeChapterRenameDTO
+ * Package: cn.timflux.storyseek.core.write.dto
+ * Description:
+ *
+ * @Author 一剑霜寒十四州
+ * @Create 2025/6/25 下午4:11
+ * @Version 1.0
+ */
+@Data
+public class VolumeChapterRenameDTO {
+    private Long id;
+    private String name;
+}
