c3909db7275d2781e76d61ff8a402cf50ba09127
Fei <xu_neu@163.com>
2025-07-01 22:39:55 +0800

    <feat>[write]:增加大纲生成

---
+++ src/main/java/cn/timflux/storyseek/core/write/mapper/OutlineMapper.java
@@ -0,0 +1,18 @@
+package cn.timflux.storyseek.core.write.mapper;
+
+import cn.timflux.storyseek.core.write.entity.Outline;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ClassName: OutlineMapper
+ * Package: cn.timflux.storyseek.core.write.mapper
+ * Description:
+ *
+ * @Author 一剑霜寒十四州
+ * @Create 2025/6/29 上午3:43
+ * @Version 1.0
+ */
+@Mapper
+public interface OutlineMapper extends BaseMapper<Outline> {
+}
